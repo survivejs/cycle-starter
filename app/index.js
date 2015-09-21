6a9ae519009a24e706f7f517cc895ca534f1731b
Juho Vepsalainen <bebraw@gmail.com>
2015-09-21 17:55:31 +0300

    Set up a timer app

--- app/index.js
+++ app/index.js
@@ -1,4 +1,23 @@
 import Cycle from '@cycle/core';
 import CycleDOM from '@cycle/dom';
 
-console.log(Cycle, CycleDOM);
+main();
+
+function main() {
+  const appElement = document.createElement('div');
+
+  document.body.appendChild(appElement);
+
+  Cycle.run(app, {
+    DOM: CycleDOM.makeDOMDriver(appElement)
+  });
+}
+
+function app() {
+  return {
+    DOM: Cycle.Rx.Observable.interval(1000)
+      .map(i => CycleDOM.h(
+        'h1', '' + i + ' seconds elapsed'
+      ))
+  };
+}
